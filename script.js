ce822069b4f3d1d162907b4c8140ae9573e31a31
govindrajeesh <govindrajeesh@gmail.com>
2021-11-29 11:49:37 +0400

    test

--- script.js
+++ script.js
@@ -109,6 +109,7 @@ function Pop(s) {
 
 function startGame() {
   window.history.pushState({game:true}, 'Game', '#game')
+  document.body=window.history.state.prevUrl
   players = [
     { name: 'X', score: 0, places: [] },
     { name: 'O', score: 0, places: [] },
